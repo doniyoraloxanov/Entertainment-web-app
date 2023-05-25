26df25a1ad45a2653eabd6bbbd30106016079588
doniyoraloxanov <doniyoraloxanov@gmail.com>
2023-05-25 15:34:00 +0500

    form validations done

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,22 +1,76 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="bg-gray-800 py-10 px-8  my-24 rounded-lg  max-w-md mx-auto">
       <h2 className="text-white text-2xl mb-8">Login</h2>
-      <form className="flex flex-col space-y-3  mb-4">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col space-y-3  mb-4"
+      >
         <input
-          type="email"
-          placeholder="Email address"
+          type="text"
+          name="email"
           className="pl-4 py-3 outline-none bg-black text-white"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          className="pl-4 py-3 outline-none  bg-black text-white "
+          {...register("email", {
+            required: true,
+            pattern: {
+              value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+              message: "Email is not valid",
+            },
+          })}
         />
 
-        <button className="block  text-white font-semibold px-4 py-3 bg-red-500 hover:bg-white hover:text-gray-500 cursor-pointer">
+        {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+
+        <input
+          type="password"
+          name="password"
+          className="pl-4 py-3 outline-none bg-black text-white"
+          {...register("password", {
+            required: true,
+            validate: {
+              checkLength: (value) => value.length >= 6,
+              matchPattern: (value) =>
+                /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s)(?=.*[!@#$*])/.test(
+                  value
+                ),
+            },
+          })}
+        />
+
+        {errors.password?.type === "required" && (
+          <p className="text-red-500">Password is required</p>
+        )}
+
+        {errors.password?.type === "checkLength" && (
+          <p className="text-red-500">
+            Password should be at least 6 charcters
+          </p>
+        )}
+
+        {errors.password?.type === "matchPattern" && (
+          <p className="text-red-500">
+            Password should contain at least one upperace letter, lowercase
+            letter,digit, and special symbol
+          </p>
+        )}
+
+        <button
+          type="submit"
+          className="block  text-white font-semibold px-4 py-3 bg-red-500 hover:bg-white hover:text-gray-500 cursor-pointer"
+        >
           Login to your account
         </button>
       </form>
